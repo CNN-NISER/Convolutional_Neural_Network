b162642327107d36d75603a9afe6483837665990
Sahel Mohammad Iqbal <sahel13miqbal@gmail.com>
2020-08-15 19:12:30 +0530

    Updated with master

--- convnet.py
+++ convnet.py
@@ -7,19 +7,18 @@ np.random.seed(0)
 class ConvNet():
 
     def __init__(self):
-        self.inputImg = None # The input data
-        self.strides = [] # The stride length of each layer for convolution
-        self.recfield = [] # The receptive field in each layer for convolution
+        self.inputImg = None  # The input data
+        self.strides = []  # The stride length of each layer for convolution
+        self.recfield = []  # The receptive field in each layer for convolution
         self.lengths = []
         self.widths = []
         self.depths = []
-        self.weights = [] # The weights for convolution filters
-        self.node = 10 # The number of nodes in the output layer
-        self.track = [] # Keeps track of layer order, i.e Conv./Pooling/FC
-        self.regLossParam = 1e-3 # Regularization strength
-        self.learning_rate = 1e-5
+        self.weights = []  # The weights for convolution filters
+        self.node = 10  # The number of nodes in the output layer
+        self.track = []  # Keeps track of layer order, i.e Conv./Pooling/FC
+        self.learning_rate = 0.005
         self.fc_weights = []
-        self.fc_output = []
+        self.output_fc = []
 
         # Adam optimization
         self.adam_m = []
@@ -31,7 +30,7 @@ class ConvNet():
     def addInput(self, inpImage):  # Assign the input image
         inpImage = np.array(inpImage)
         self.inputImg = inpImage
-        if(len(inpImage.shape)<3):
+        if(len(inpImage.shape) < 3):
             num3 = 1
             numrows = inpImage.shape[0]
             numcols = inpImage.shape[1]
@@ -65,7 +64,7 @@ class ConvNet():
 
         #b = np.zeros((1, stre))
         for i in range(f):
-            W.append(np.random.randn(prevd, r, r))
+            W.append(np.random.randn(prevd, r, r) / (r*r))
             M.append(np.zeros((prevd, r, r)))
             V.append(np.zeros((prevd, r, r)))
 
@@ -75,7 +74,7 @@ class ConvNet():
 
         # The dimensions of the layer after convolution with the above weight array
         numrows = (prevw - r)/s + 1
-        numcols = (prevl - r)/s +1
+        numcols = (prevl - r)/s + 1
         num3 = f
 
         # Store them
@@ -130,7 +129,7 @@ class ConvNet():
         len_input_fc = len(input_fc)
 
         # Initialise the weights and biases for the FC layer
-        self.fc_weights = np.random.randn(len_input_fc, n_nodes)
+        self.fc_weights = np.random.randn(len_input_fc, n_nodes) / (len_input_fc)
         #self.fc_bias = np.zeros(n_nodes)
 
         # Store them
@@ -157,7 +156,7 @@ class ConvNet():
         """
         Returns the data loss. Cross-Entropy loss function (Softmax Classifier).
         """
-        # L2 loss
+        # Softmax
         loss = 0
         sum = 0
         for i in range(len(predResults)):
@@ -177,16 +176,16 @@ class ConvNet():
         w = int(w)
         l = int(l)
         volOutput = np.zeros((d, w, l))
-        if(len(W.shape)<4):
+        if(len(W.shape) < 4):
             f = 1
         else:
             f = W.shape[0]
 
-        for i in range(f):   #Run loop to create f-filters
-            for k in range(w):  #Convolve around width
-                for m in range(l):   #Convolve around length
-                    #for j in range(d):   #Run over entire depth of prevOut volume
-                    volOutput[i][k][m] += np.sum(np.multiply(W[i][:][:][:], prevOut[:, k*s: k*s + r, m*s: m*s + r])[:,:,:])
+        for i in range(f):  # Run loop to create f-filters
+            for k in range(w):  # Convolve around width
+                for m in range(l):  # Convolve around length
+                    # for j in range(d):   #Run over entire depth of prevOut volume
+                    volOutput[i][k][m] += np.sum(np.multiply(W[i][:][:][:], prevOut[:, k*s: k*s + r, m*s: m*s + r])[:, :, :])
 
         volOutput = np.array(volOutput)
         return volOutput
@@ -212,26 +211,26 @@ class ConvNet():
 
     def FCOutput(self, prevOut, W, s, r, d, w, l):
         """
-        Creates a fully connected layer; implements forward pass and
-        backpropagation.
-        n_nodes - the no.of nodes in the fully connected layer.
-        input_fc - the input to the fully connected layer.
+        Implements forward pass for the FC layer. Uses a softmax Classifier.
+        n_nodes - the no.of nodes in the fully connected layer. 
         """
         # flatten the input
         prevOut = prevOut.flatten()
         #len_input_fc = len(prevOut)
 
-        totals = np.dot(prevOut, W) #+ self.fc_bias
+        totals = np.dot(prevOut, W)  # + self.fc_bias
+        # Softmax
+        exp_totals = np.exp(totals)
 
         # Output from the FC layer
-        self.output_fc = self.activFunc(totals)
+        self.output_fc = exp_totals / (np.sum(exp_totals, axis=0))
         return self.output_fc
 
     def getVolumeOutput(self, n):
         """
         Returns the output of the nth volume of the ConvNet.
         """
-        penLayer = len(self.weights) - 1 # The penultimate volume
+        penLayer = len(self.weights) - 1  # The penultimate volume
 
         # h stores the output of the current layer
         h = np.array(self.inputImg)
@@ -263,21 +262,33 @@ class ConvNet():
             l = self.lengths[n]
             return self.FCOutput(h, W, s, r, d, w, l)
 
-    def FCGD(self, index, trueResults): #FC layer Gradient Descent
+    def FCGD(self, index, trueResults):  # FC layer Gradient Descent
         input_fc = self.getVolumeOutput(index - 1)
 
-        # store the shape of input to be used for backpropagation
+        # Store the shape of input before flattening (to be used for backpropagation)
         input_fc_shape = input_fc.shape
 
-        # flatten the input
+        # Flatten the input
         input_fc = input_fc.flatten()
 
+        # Get the weights and the totals of the fixed layer
         W = self.weights[index - 1]
         totals = np.dot(input_fc, W)
 
-        h = 0.001 * np.ones(self.output_fc.shape)
-        d_L_d_out = (self.dataLoss(self.output_fc + h, trueResults) - self.dataLoss(self.output_fc - h, trueResults))/(2*h)
-        d_out_d_t = np.where(totals <= 0, 0, 1)
+        # Calculating d_L_d_out
+        correct = np.argmax(trueResults)
+        d_L_d_out = np.zeros_like(self.output_fc)
+        d_L_d_out[correct] = -1 / (self.output_fc[correct])
+
+        # Calculating d_out_d_t
+        exp_totals = np.exp(totals)
+        sum_exp_totals = np.sum(exp_totals)
+
+        d_out_d_t = np.zeros_like(self.output_fc)
+        d_out_d_t = -exp_totals[correct] * (exp_totals / (sum_exp_totals ** 2))
+        d_out_d_t[correct] = exp_totals[correct] * ((sum_exp_totals - exp_totals[correct])/(sum_exp_totals ** 2))
+
+        # Other necessary gradients
         d_t_d_w = input_fc
         d_t_d_inputs = W
         d_L_d_t = d_L_d_out * d_out_d_t
@@ -295,53 +306,54 @@ class ConvNet():
 
         W -= self.learning_rate * d_L_d_w
         self.weights[index - 1] = W
+
         return d_L_d_inputs_final
 
-	def PoolGD(self, dLdOut, index):
-		"""
-		Function that backpropagates gradients through the MaxPooling layer
-		dLdOut is the differential of Loss wrt the Output where Output here refers to the output of the MaxPooling layer
-		This function thus finds dLdI which is the differential of Loss wrt the Input where Input here refers to input to MaxPool layer.
-		"""
-		input_vol = self.getVolumeOutput(index - 1)
-		s = self.strides[index - 1]
-		r = self.recfield[index - 1]
-		d = dLdOut.shape[0]
-		w = dLdOut.shape[1]
-		l = dLdOut.shape[2]
+    def PoolGD(self, dLdOut, index):
+        """
+        Function that backpropagates gradients through the MaxPooling layer
+        dLdOut is the differential of Loss wrt the Output where Output here refers to the output of the MaxPooling layer
+        This function thus finds dLdI which is the differential of Loss wrt the Input where Input here refers to input to MaxPool layer.
+        """
+        input_vol = self.getVolumeOutput(index - 1)
+        s = self.strides[index - 1]
+        r = self.recfield[index - 1]
+        d = dLdOut.shape[0]
+        w = dLdOut.shape[1]
+        l = dLdOut.shape[2]
 
-		#Convert the numbers to int, as the for loops below will report errors if this is not done 
-		d = int(d)
-		w = int(w)
-		l = int(l)
+        # Convert the numbers to int, as the for loops below will report errors if this is not done
+        d = int(d)
+        w = int(w)
+        l = int(l)
 
-		#Keep track of the depth and spatial indices of where the maximum element is, in the sub arrays taken for pooling
-		d_ind = []
-		spatial_ind = []
-		#Keep track of which sub array is being taken for max pooling
-		track_w = []
-		track_l = []
+        # Keep track of the depth and spatial indices of where the maximum element is, in the sub arrays taken for pooling
+        d_ind = []
+        spatial_ind = []
+        # Keep track of which sub array is being taken for max pooling
+        track_w = []
+        track_l = []
 
-		dLdI = np.zeros((int(self.depths[index - 1]), int(self.lengths[index - 1]), int(self.widths[index - 1])))
-		replace = dLdOut.flatten()
-		for j in range(d):
-			for k in range(w):
-				for m in range(l):
-					spatial_ind.append(np.where(input_vol[j, k*r: (k + 1)*r, m*r: (m + 1)*r] == input_vol[j, k*r: (k + 1)*r, m*r: (m + 1)*r].max()))
-					track_l.append(m)
-					track_w.append(k)
-					d_ind.append(j)
+        dLdI = np.zeros((int(self.depths[index - 1]), int(self.lengths[index - 1]), int(self.widths[index - 1])))
+        replace = dLdOut.flatten()
+        for j in range(d):
+            for k in range(w):
+                for m in range(l):
+                    spatial_ind.append(np.where(input_vol[j, k*r: (k + 1)*r, m*r: (m + 1)*r] == input_vol[j, k*r: (k + 1)*r, m*r: (m + 1)*r].max()))
+                    track_l.append(m)
+                    track_w.append(k)
+                    d_ind.append(j)
 
-		#Initialise correct values in dLdI array
-		for i in range(len(replace)):
-			width = spatial_ind[i][0][0]    # Note the (width) spatial index of the maximum element of the sub array 
-			width += track_w[i]*r  # Add the (width) location depending on which sub array was taken for max pooling
-			length = spatial_ind[i][1][0]   # Note the (length) spatial index of the maximum element of the sub array 
-			length += track_l[i]*r  # Add the (length) location depending on which sub array was taken for max pooling
-			depth = d_ind[i]  # Note the depth index of the maximum element of the sub array 
-			dLdI[depth][width][length] = replace[i]
+        # Initialise correct values in dLdI array
+        for i in range(len(replace)):
+            width = spatial_ind[i][0][0]  # Note the (width) spatial index of the maximum element of the sub array
+            width += track_w[i]*r  # Add the (width) location depending on which sub array was taken for max pooling
+            length = spatial_ind[i][1][0]  # Note the (length) spatial index of the maximum element of the sub array
+            length += track_l[i]*r  # Add the (length) location depending on which sub array was taken for max pooling
+            depth = d_ind[i]  # Note the depth index of the maximum element of the sub array
+            dLdI[depth][width][length] = replace[i]
 
-		return dLdI
+        return dLdI
 
     # Helper functions for convBackProp()
     def convolve(self, inputLayer, convFilter):
@@ -363,7 +375,7 @@ class ConvNet():
         # Convolving
         for row in range(l):
             for col in range(l):
-                output_matrix[row][col] = np.sum(np.multiply(inputLayer[row:row+f,col:col+f], convFilter))
+                output_matrix[row][col] = np.sum(np.multiply(inputLayer[row:row+f, col:col+f], convFilter))
 
         return output_matrix
 
@@ -380,8 +392,8 @@ class ConvNet():
         padding = f - 1
         new_dim = n + 2*padding
 
-        padded_input = np.zeros((new_dim, new_dim))
-        padded_input[padding:new_dim - padding, padding:new_dim - padding] = inputLayer
+        padded_input = np.zeros([new_dim, new_dim])
+        padded_input[padding:new_dim - padding,padding:new_dim - padding] = inputLayer
 
         # Now convolve padded_input with convFilter
         output_matrix = self.convolve(padded_input, convFilter)
@@ -395,16 +407,16 @@ class ConvNet():
         """
         return np.rot90(matrix, 2)
 
-    def ConvGD(self, dLdoutput, index, t):
+    def ConvGD(self, dLdoutput, index):
         """
         Function that backpropagates through a convolutional layer.
         index = index of the current layer
         dLdoutput = Gradient of the loss function wrt the output of the current layer (channel, row, col)
         Returns dLdinput.
-        t is the iteration counter.
         """
-        X = self.getVolumeOutput(index-1) # Input to the current layer (channel, row, col)
-        W = self.weights[index - 1] # Weights of the current layer (numFilter, channel, row, col)
+        X = self.getVolumeOutput(index-1)  # Input to the current layer (channel, row, col)
+        # Weights of the current layer (numFilter, channel, row, col)
+        W = self.weights[index - 1]
         M = self.adam_m[index - 1]
         V = self.adam_v[index - 1]
 
@@ -444,7 +456,6 @@ class ConvNet():
 
         return dLdX
 
-
     def backPropagation(self, input, trueResults):
         """
         Updates weights by carrying out backpropagation.
@@ -452,12 +463,13 @@ class ConvNet():
         """
         for i in range(len(input)):
             self.inputImg = np.array(input[i])
-            if(len(self.inputImg.shape)<3):
+            if(len(self.inputImg.shape) < 3):
                 a = self.inputImg
                 self.inputImg = a.reshape(1, a.shape[0], a.shape[1])
             # Called once so that all weights are initialised, just in case if not done before
             out = self.getVolumeOutput(len(self.weights))
-            nPrev = len(self.weights) # Index keeping track of the previous layer
+            # Index keeping track of the previous layer
+            nPrev = len(self.weights)
             doutput = self.FCGD(nPrev, trueResults[i])
             nPrev -= 1
 
@@ -466,10 +478,8 @@ class ConvNet():
                 if(self.track[nPrev - 1] == 'p'):
                     dhidden = self.PoolGD(doutput, nPrev)
                 else:
-                    # i is taken as the iteration counter
-                    # since the weights are modified for each i
                     dhidden = self.ConvGD(doutput, nPrev, i+1)
-                doutput = dhidden # Move to the previous layer
+                doutput = dhidden  # Move to the previous layer
                 nPrev -= 1
 
     def train(self, input, Y, epochs):
@@ -481,7 +491,7 @@ class ConvNet():
         # Run backPropagation() 'epochs' number of times.
         for i in range(epochs):
             self.backPropagation(input, Y)
-            print('Epoch Number: ',i + 1,' done.')
+            print('Epoch Number: ', i + 1, ' done.')
         print("Training Complete.")
 
     def accuracy(self, X, Y):
@@ -493,13 +503,13 @@ class ConvNet():
         correct = 0
         for i in range(len(X)):
             self.inputImg = np.array(X[i])
-            if(len(self.inputImg.shape)<3):
+            if(len(self.inputImg.shape) < 3):
                 a = self.inputImg
                 self.inputImg = a.reshape(1, a.shape[0], a.shape[1])
                 y.append(self.getVolumeOutput(len(self.weights)))
         Y = np.array(Y)
         y = np.array(y)
-        if (np.max(y)==0):
+        if (np.max(y) == 0):
             y /= 1.0
         else:
             y /= np.max(y)
@@ -508,4 +518,4 @@ class ConvNet():
             if (np.argmax(y[i]) == correct):
                 cor += 1
         cor /= len(Y)
-        print('Accuracy = ',cor*100)
+        print('Accuracy = ', cor*100)
